51f3ecc5c64fbde448ae459c2b3b6304600ef5ff
sebastian Zarate <sebaszarate2005@gmail.com>
2025-09-26 01:07:06 -0500

    se crea los metodos post para usuario y para proveedor

--- src/poliSong_DBPost.py
+++ src/poliSong_DBPost.py
@@ -28,7 +28,7 @@ ensure_schema()
 def detalle_usuario(usuario_id):
     with get_conn() as conn:
         fila = conn.execute(
-            "SELECT id_usuario, nombre, contrasena FROM usuario WHERE id_usuario = ?",
+            "SELECT u.id_usuario, u.nombre, u.contrasena, t.telefono, c.correo FROM usuario u LEFT JOIN telefono t ON u.id_usuario = t.id_us LEFT JOIN correo c ON u.id_usuario = c.id_us ORDER BY u.id_usuario",
             (usuario_id,)
         ).fetchone()
     if fila is None:
@@ -36,7 +36,8 @@ def detalle_usuario(usuario_id):
     return jsonify(dict(fila)), 200
 
 # -------- POST: crear --------
-@app.route("/crear", methods=["POST"])
+#TABLA USUARIO
+@app.route("/crear/usuario", methods=["POST"])
 def crear_usuario():
     if not request.is_json:
         return jsonify(error="Content-Type debe ser application/json"), 415
@@ -47,8 +48,10 @@ def crear_usuario():
 
     nombre = data.get("nombre")
     contrasena = data.get("contrasena")
+    telefono = data.get("telefono")
+    correo = data.get("correo")
 
-    faltan = [k for k in ("nombre", "contrasena") if k not in data]
+    faltan = [k for k in ("nombre", "contrasena","telefono", "correo") if k not in data]
     if faltan:
         return jsonify(error=f"Faltan campos: {', '.join(faltan)}"), 400
 
@@ -63,6 +66,18 @@ def crear_usuario():
     contrasena = contrasena.strip()
     if not contrasena or len(contrasena) > 100:
         return jsonify(error="contrasena no puede estar vacía (<= 100 chars)"), 422
+    
+    if not isinstance(telefono, str):
+        return jsonify(error="telefono debe ser string"), 422
+    telefono = telefono.strip()
+    if not telefono or len(telefono) > 100:
+        return jsonify(error="telefono no puede estar vacía (<= 100 chars)"), 422
+    
+    if not isinstance(correo, str):
+        return jsonify(error="correo debe ser string"), 422
+    correo = correo.strip()
+    if not correo or len(correo) > 100:
+        return jsonify(error="correo no puede estar vacía (<= 100 chars)"), 422
 
     with get_conn() as conn:
         cur = conn.execute(
@@ -71,9 +86,75 @@ def crear_usuario():
         )
         nuevo_id = cur.lastrowid
 
-    resp = jsonify({"id_usuario": nuevo_id, "nombre": nombre, "contrasena": contrasena})
+        cur = conn.execute(
+            "INSERT INTO telefono (id_us, telefono) VALUES (?, ?)",
+            (nuevo_id, telefono)
+        )
+        cur = conn.execute(
+            "INSERT INTO correo (id_us, correo) VALUES (?, ?)",
+            (nuevo_id, correo)
+        )
+
+    resp = jsonify({"id_usuario": nuevo_id, "nombre": nombre, "contrasena": contrasena, "correo":correo, "telefono": telefono})
     resp.status_code = 201
     resp.headers["Location"] = f"/usuarios/{nuevo_id}"
+    return resp
+
+#TABLA PROVEDOR
+@app.route("/crear/proveedor", methods=["POST"])
+def crear_proveedor():
+    if not request.is_json:
+        return jsonify(error="Content-Type debe ser application/json"), 415
+
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify(error="JSON malformado o vacío"), 400
+
+    nombre = data.get("nombre")
+    telefono = data.get("telefono")
+    correo = data.get("correo")
+
+    faltan = [k for k in ("nombre", "telefono", "correo") if k not in data]
+    if faltan:
+        return jsonify(error=f"Faltan campos: {', '.join(faltan)}"), 400
+
+    if not isinstance(nombre, str):
+        return jsonify(error="nombre debe ser string"), 422
+    nombre = nombre.strip()
+    if not nombre or len(nombre) > 100:
+        return jsonify(error="nombre no puede estar vacío (<= 100 chars)"), 422
+    
+    if not isinstance(telefono, str):
+        return jsonify(error="telefono debe ser string"), 422
+    telefono = telefono.strip()
+    if not telefono or len(telefono) > 100:
+        return jsonify(error="telefono no puede estar vacía (<= 100 chars)"), 422
+    
+    if not isinstance(correo, str):
+        return jsonify(error="correo debe ser string"), 422
+    correo = correo.strip()
+    if not correo or len(correo) > 100:
+        return jsonify(error="correo no puede estar vacía (<= 100 chars)"), 422
+
+    with get_conn() as conn:
+        cur = conn.execute(
+            "INSERT INTO proveedor (nombre) VALUES (?)",
+            (nombre,)
+        )
+        nuevo_id = cur.lastrowid
+
+        cur = conn.execute(
+            "INSERT INTO telefono_proveedor (id_proveedor, telefono) VALUES (?, ?)",
+            (nuevo_id, telefono)
+        )
+        cur = conn.execute(
+            "INSERT INTO correo_proveedor (id_proveedor, correo) VALUES (?, ?)",
+            (nuevo_id, correo)
+        )
+
+    resp = jsonify({"id_usuario": nuevo_id, "nombre": nombre, "correo":correo, "telefono": telefono})
+    resp.status_code = 201
+    resp.headers["Location"] = f"/proveedores/{nuevo_id}"
     return resp
 
 if __name__ == "__main__":
